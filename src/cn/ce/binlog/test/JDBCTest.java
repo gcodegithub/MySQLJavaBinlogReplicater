ac4e1d7bab73c69d81cef0d97ba6c5e2d3ea1052
ppm10103 <ppm10103@gmail.com>
2013-12-19 11:18:52 +0800

    profiler

--- src/cn/ce/binlog/test/JDBCTest.java
+++ src/cn/ce/binlog/test/JDBCTest.java
@@ -7,28 +7,34 @@ import java.sql.SQLException;
 
 public class JDBCTest {
 
-	public static void update(String newValue) throws Exception {
+	public static void update() throws Exception {
 		Connection conn = null;
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			conn = DriverManager.getConnection(
-					"jdbc:mysql://192.168.215.1:3306/test", "canal", "canal");
-			conn.setAutoCommit(false);
-			PreparedStatement stat = conn
-					.prepareStatement("update log4j set CODE=?");
-			Long startTime = System.currentTimeMillis();
-			stat.setObject(1, newValue);
-			stat.executeUpdate();
-			conn.commit();
-			long endTime = System.currentTimeMillis();
-			System.out.println(endTime - startTime);
-		} finally {
-			if (conn != null)
-				conn.close();
+		int i = 0;
+		while (true) {
+			i++;
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				conn = DriverManager.getConnection(
+						"jdbc:mysql://192.168.215.1:3306/test", "canal",
+						"canal");
+				conn.setAutoCommit(false);
+				PreparedStatement stat = conn
+						.prepareStatement("update log4j set CODE=?");
+				Long startTime = System.currentTimeMillis();
+				stat.setObject(1, startTime+(i*5));
+				stat.executeUpdate();
+				conn.commit();
+				long endTime = System.currentTimeMillis();
+				System.out.println(endTime - startTime);
+			} finally {
+				if (conn != null)
+					conn.close();
+			}
+			Thread.sleep(500);
 		}
 	}
 
-	public static void insert(int start, int num) throws Exception {
+	public static void insert() throws Exception {
 		Connection conn = null;
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
@@ -37,19 +43,30 @@ public class JDBCTest {
 			conn.setAutoCommit(false);
 			PreparedStatement stat = conn
 					.prepareStatement("insert into log4j values(?,?,?,?)");
-			Long startTime = System.currentTimeMillis();
-			for (int i = start; i < num; i++) {
-				stat.setObject(1, System.currentTimeMillis() + i);
-				stat.setObject(2, i);
-				stat.setObject(3, i);
-				stat.setObject(4, i);
-				stat.addBatch();
+			PreparedStatement stat1 = conn
+					.prepareStatement("insert into log5j values(?,?,?,?)");
+
+			for (int j = 0; j < 2; j++) {
+				for (int i = 0; i < 1000; i++) {
+					stat.setObject(1, System.currentTimeMillis() + i);
+					stat.setObject(2, i);
+					stat.setObject(3, i);
+					stat.setObject(4, i);
+					stat.addBatch();
+					stat1.setObject(1, System.currentTimeMillis() + i);
+					stat1.setObject(2, i);
+					stat1.setObject(3, i);
+					stat1.setObject(4, i);
+					stat1.addBatch();
+				}
+				stat.executeBatch();
+				stat.clearBatch();
+				stat1.executeBatch();
+				stat1.clearBatch();
+				conn.commit();
+				Thread.sleep(1000);
+				System.out.println("----------一轮循环---------------");
 			}
-			stat.executeBatch();
-			stat.clearBatch();
-			conn.commit();
-			long endTime = System.currentTimeMillis();
-			System.out.println(endTime - startTime);
 		} finally {
 			if (conn != null)
 				conn.close();
@@ -58,8 +75,8 @@ public class JDBCTest {
 
 	public static void main(String[] args) {
 		try {
-			JDBCTest.insert(0, 10000);
-			JDBCTest.update("update" + System.currentTimeMillis());
+			JDBCTest.insert();
+//			JDBCTest.update();
 			System.out.println("----------OVER---------------");
 		} catch (Exception e) {
 			e.printStackTrace();
